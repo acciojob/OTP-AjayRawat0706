635c7c8a4eae5169b1b90f6afedcd65463685cf8
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-24 22:50:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+
+codes.forEach((val, index) => {
+
+    val.addEventListener("input", (e) => {
+        let value1 = e.target.value
+        if (value1) {
+            if (index < codes.length - 1) {
+                codes[index + 1].focus()
+            }
+        }
+
+    })
+
+    val.addEventListener("keydown", (e) => {
+        let keypress = e.key
+        let value2 = e.target.value
+        if (keypress == "Backspace") {
+            if(value2=="" &&index>0){
+
+                codes[index-1].focus()
+            }
+
+
+           
+        }
+
+    })
+    
+  
+    window.addEventListener("DOMContentLoaded", () => {
+        codes[0].focus();
+      });
+})
